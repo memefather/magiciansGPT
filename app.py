e3a0b9359ecc50c5d78c3f7eeb1339a6c1357065
memefather <142188887+memefather@users.noreply.github.com>
2023-09-10 14:15:27 -0700

    Update app.py

--- app.py
+++ app.py
@@ -333,7 +333,7 @@ if prompt := st.chat_input("How can I help?"):
         ):
             full_response += response.choices[0].delta.get("content", "")
             message_placeholder.markdown(full_response + "▌")
-        message_placeholder.markdown(full_response.replace('$',''))
+        message_placeholder.markdown(full_response)
     st.session_state.messages.append({"role": "assistant", "content": full_response})
 
 
