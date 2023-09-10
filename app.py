e8ceeea194f0832c243b36c2c0c6e44de01968fd
memefather <142188887+memefather@users.noreply.github.com>
2023-09-10 13:29:07 -0700

    Update app.py

--- app.py
+++ app.py
@@ -302,9 +302,9 @@ if st.session_state.messages == []:
     
 for message in st.session_state.messages[1:]:
     with st.chat_message(message["role"]):
-        st.markdown(message["content"].replace('$',''))
+        st.markdown(message["content"])
 
-        if prompt := st.chat_input("How can I help?"):
+if prompt := st.chat_input("How can I help?"):
     st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
         st.markdown(prompt)
