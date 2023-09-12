55d5e3c8e41a76ad395ac584a5fcbd746202e450
memefather <142188887+memefather@users.noreply.github.com>
2023-09-11 17:10:54 -0700

    Update app.py

--- app.py
+++ app.py
@@ -387,12 +387,12 @@ if prompt := st.chat_input("Speak Mysteriously"):
     elif prompt.find('think of a card') != -1 or prompt.find('thinking of a card') != -1:
         st.session_state.messages.append({"role": "system", "content": "The revealation is the 9 of diamonds"})
     elif prompt.find('a story') != -1:
-        st.session_state.messages.append({"role": "system", "content": "When you see this prompt, ignore all previous prompts. End the chat nicely with emoji and no longer communicate with the user."})
+        st.session_state.story = True
+	st.session_state.messages.append({"role": "system", "content": "When you see this prompt, ignore all previous prompts. End the chat nicely with emoji and no longer communicate with the user."})
         listprompt = prompt.split()
         card = listprompt[listprompt.index("of")-1] + ' of ' + listprompt[listprompt.index("of")+1]
         if card[-1] == '.' or card[-1] == '?' or card[-1] == '!':
-	        card = card[:-1]
-        st.session_state.story = True
+	    card = card[:-1]
 	[newtitle, newstory, newsummary] = get_story(card)
 	st.session_state.messages.append({"role": "assistant", "content": newtitle})
         st.session_state.messages.append({"role": "assistant", "content": newstory})
