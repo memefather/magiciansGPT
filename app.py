9aeb6174405b74de492be569bd0cdd114ab06f78
memefather <142188887+memefather@users.noreply.github.com>
2023-09-11 17:22:41 -0700

    Update app.py

--- app.py
+++ app.py
@@ -405,18 +405,11 @@ if prompt := st.chat_input("Speak Mysteriously"):
             st.session_state.story == False
         else:
             message_placeholder = st.empty()
-    	    full_response = ""
-    	    for response in openai.ChatCompletion.create(
-    		    model=st.session_state["openai_model"],
-    		    messages=[
-    		    {"role": m["role"], "content": m["content"]}
-    		    for m in st.session_state.messages
-    		    ],
-    		    stream=True,
-    		    ):
-    		    full_response += response.choices[0].delta.get("content", "")
-    		    message_placeholder.markdown(full_response + "▌")
-    	    message_placeholder.markdown(full_response)
+            full_response = ""
+            for response in openai.ChatCompletion.create(model=st.session_state["openai_model"],messages=[{"role": m["role"], "content": m["content"]} for m in st.session_state.messages],stream=True):
+                full_response += response.choices[0].delta.get("content", "")
+                message_placeholder.markdown(full_response + "▌")
+            message_placeholder.markdown(full_response)
     st.session_state.messages.append({"role": "assistant", "content": full_response})
 
 
